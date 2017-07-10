1ef5660795ae9d73734a45b22b74ab866635d68e
Matt Pitts <mattpittsmusic@gmail.com>
2017-07-10 14:42:33 -0600

    added regex email validation

--- api/validate.js
+++ api/validate.js
@@ -1,7 +1,8 @@
 module.exports = {
 	user(user) {
-		const validEmail = typeof user.email == 'string' &&
-						user.email.trim() != '';
+		const filter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		const validEmail = filter.test(user.email);
+
 		const validPassword = typeof user.password == 'string' &&
 						user.password.trim() != '' &&
 						user.password.trim().length >= 6;
